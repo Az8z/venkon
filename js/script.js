ee24ffc10fe90170e748ed27b4fb024783c56500
Aziz Toshpulatov <azidi.t44@gmail.com>
2018-07-09 02:14:04 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+// SMOOTH SCROLL
+
+$(function() {
+
+	"use strict";
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+//WOW JS
+$(document).ready(function() {
+
+    "use strict";
+
+    new WOW().init();
+});
